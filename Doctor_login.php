289bb6633d0d674dfe34b46cbdaa7c3887763bdc
Dinithikw <Dinithikw@users.noreply.github.com>
2021-01-02 21:26:09 +0530

    login pages updated

--- Doctor_login.php
+++ Doctor_login.php
@@ -1,38 +1,30 @@
 <html>
 <head>
-	<title>Patient Log In</title>
-		<link rel="Stylesheet" type="text/css" href="css/style1.css">
+	<title>Doctor Log In</title>
+		<link rel="Stylesheet" type="text/css" href="css/login.css">
 </head>
 <body>
-
 	<header>
 			<div class="header">
 				<img src="img/logo.jpg"alt="logo"style="width:150px;height:150;font-size:30px ">
 			<a style="float:right;padding-right:450px;padding-top:30px;font-size:50px;">Rajarata Medical Center</a>
 			</div>
 		</header>
-
-
 		<div class="loginbox">
-			<img src="img/Patient logo 1.jpg" class="avatar">
-			<h1>Patient Login</h1>
+			<img src="img/user icon.png" class="avatar">
+			<h1>Login Here</h1>
 			<form>
-					<p>User Name</p>
+					<p>Doctor User Name</p>
 					<input type="text" name="" placeholder="Enter User Name">
 					<p>Password</p>
 					<input type="password" name="" placeholder="Enter Password">
 					<input type="submit" name="" value="Login">
-					<a href="#">Don't have an Account?.</a>
 				</form>
 			</div>
-
-
 			<br>
 			<br>
 			<br>
 			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-
-
 			<Footer>
 							<div class="footer">
 								<h2>Footer here</h2>

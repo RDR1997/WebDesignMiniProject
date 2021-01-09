7adc16c885b7b1409e528293cd4ba67c7233c4c9
Randika Ranatunga <75309593+RDR1997@users.noreply.github.com>
2021-01-09 18:04:36 +0530

    admin dashboard update

---
+++ admindashbord.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Admin Dashbord</title>
+		<link rel="stylesheet" type="text/css" href="css/admindashbord.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+	
+	<!-- navigation bar------------------------------------------------------------------------------------------------------------------------------------- -->
+			<ul>
+				<img src="img/logo.png"alt="logo"style="width:55px;height:55px; padding-left: 10px;"align="left";>
+				<li class="logo">My Doctor Hospitals
+				
+				</li>
+				
+				<li class="nav"><a href="index.php">Logout</a></li>
+				<li class="nav"><a href="contact.php">Contact</a></li>
+				<li class="nav"><a href="about.php">About us</a></li>
+				<li class="nav"><a href="services.php">Services</a></li>
+				<li class="nav"><a href="index.php">Home</a></li>
+			</ul>
+			
+		<!--profile line--------------------------------------------------------------------------------------------------------------------------------------->	
+		<div class="split left">
+			<div class="centered">
+				<img src="img/img_avatar2.png" alt="Avatar woman">
+				<h2>Jane Flex</h2>
+				<p>Some text.</p>
+				<p>Some text.</p>
+				<p>Some text.</p>
+				<p>Some text.</p>
+				<p>Some text.</p>
+			</div>
+		</div>	
+		
+		
+			<table class="table1">
+				<tr>
+					<th>Manage Doctors</th>
+					<th>Manage Patient</th>
+					<th>Manage Appointments</th>
+				</tr>
+				<tr>
+					<td><a href="#1">View Doctors</a></td>
+					<td><a href="#1">#1</a></td>
+					<td><a href="#1">#1</a></td>
+					
+				</tr>
+				<tr>
+					<td><a href="#2">Update Doctors</a></td>
+					<td><a href="#2">#2</a></td>
+					<td><a href="#2">#2</a></td>
+					
+				</tr>
+				<tr>
+					<td><a href="addDoc.php">Add Doctors</a></td>
+					<td><a href="#3">#3</a></td>
+					<td><a href="#3">#3</a></td>
+					
+				</tr>
+				<tr>
+					<td><a href="#4">Remove Doctors</a></td>
+					<td><a href="#4">#4</a></td>
+					<td><a href="#4">#4</a></td>
+					
+				</tr>
+</table>
+		<br><br><br><br><br><br>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	<!-- Footer------------------------------------------------------------------------------------------------------------------------------------------------- -->
+			<footer>
+		
+			<div class="row">
+			
+				<div class="column5";>
+					<h1 align="left";>My Doctor Hospitals(pvt)Ltd</h1>
+					<img src="img/logo.png"alt="logo"style="width:150px;height:150px;"align="left";>
+				</div>
+
+				<div class="column6">
+					<h2 align="left";>Contact Us</h2>
+					<table>
+						<tr style="line-height: 1em;">
+							<td><i class="fa"><img src="icon/7.png" style="height: 30px; width: 30px;"></i></td>
+							<td>My Doctor Hospitals(pvt)Ltd<br>Anuradhapura<br>Sri Lanka</td>
+						</tr>
+						<tr style="line-height: 1em;">
+							<td><i class="fa"><img src="icon/6.png" style="height: 30px; width: 30px;"></i></td>
+							<td>+94-55 5555555</td>
+						</tr>
+						<tr style="line-height: 1em;">
+							<td><i class="fa"><img src="icon/4.png" style="height: 30px; width: 30px;"></i></td>
+							<td><a href="#">www.mydoctorhospitals.abc.lk</a></td>
+						</tr>
+						<tr style="line-height: 1em;">
+							<td><i class="fa"><img src="icon/1.png" style="height: 30px; width: 30px;"></i></td>
+							<td><a href="#">my_doc@abc.lk</a></td>
+						<tr>
+						<tr style="line-height: 1em;">
+							<td><i class="fa"><img src="icon/3.png" style="height: 30px; width: 30px;"></i></td>
+							<td>+94-55 5555555</td>
+						</tr>
+					</table>
+				</div>
+	
+				<div class="column7">
+						<div class="row">
+							<div class="links">
+							<h2>Navigate</h2>
+								<a href="index.html" style="font-size: 12px; line-height: 2em;">home</a><br>
+								<a href="services.html" style="font-size: 12px; line-height: 2em;">Services</a><br>
+								<a href="about_us.html" style="font-size: 12px; line-height: 2em;">About Us</a><br>
+								<a href="contact.html" style="font-size: 12px; line-height: 2em;">Contact Us</a><br>
+								<hr>
+							</div>
+						</div>
+						<div class="row">
+							<div class="connect">
+								<h2>Find Us:</h2>
+					
+								<table>
+									<tr>
+										<td><a href="#"><img src="icon/2.png" style="height: 50px; width: 50px;"></a></td>
+										<td><a href="#"><img src="icon/5.png" style="height: 50px; width: 50px;"></a></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+				</div>
+
+			</div>
+	
+			</footer>		
+	
+
+	</table>
+	
+	</body>
+</html>

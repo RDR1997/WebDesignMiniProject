a77335da6927f99bb8239ea2163e5ea5d342730e
Randika Ranatunga <75309593+RDR1997@users.noreply.github.com>
2021-01-10 12:12:29 +0530

    admin dashboard

--- addDoc.php
+++ addDoc.php
@@ -14,14 +14,7 @@
 				
 				</li>
 				
-				<li class="login_dropdown"><!--when mouse point put on Login aren on Nav.bar it will drop down more options-->
-					<button class="dropbtn">Login</button>
-					<div class="login_dropdown-content">
-						<a href="Admin_login.php">Login as Admin</a>
-						<a href="Doctor_login.php">Login as Doctor</a>
-						<a href="Patient_login.php">Login as Patient</a>
-					</div>
-				</li>
+				<li class="nav"><a href="index.php">Logout</a></li>
 				<li class="nav"><a href="contact.php">Contact</a></li>
 				<li class="nav"><a href="about.php">About us</a></li>
 				<li class="nav"><a href="services.php">Services</a></li>
@@ -74,6 +67,7 @@
 			<div style="text-align:center"> 
 			<input type="submit">
 			<input type="reset">
+			
 			</div>
 		</div>
 	</form>	

0f973e9f03e8d3e11d9157fce6d76cd783e955a5
Thinun <75312054+Thinun@users.noreply.github.com>
2021-01-10 12:13:17 +0530

    t

--- patient_login.php
+++ patient_login.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Patient Log In</title>
 		<link rel="Stylesheet" type="text/css" href="css/style1.css">
+		<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<header>
@@ -18,7 +19,7 @@
 					<input type="text" name="" placeholder="Enter User Name">
 					<p>Password</p>
 					<input type="password" name="" placeholder="Enter Password">
-					<input type="submit" name="" value="Login">
+					<input type="submit" name="" value="Login"><br>
 					<a href="#">Don't have an Account?.</a>
 				</form>
 			</div>
@@ -26,11 +27,68 @@
 			<br>
 			<br>
 			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-			<Footer>
-							<div class="footer">
-								<h2>Footer here</h2>
+			<footer>
+
+				<div class="row">
+
+					<div class="column5">
+						<h1>My Doctor Hospitals(pvt)Ltd</h1>
+					</div>
+
+					<div class="column6">
+						<h2>Contact Us</h2>
+						<table>
+							<tr style="line-height: 1em;">
+								<td><i class="fa"><img src="icon/7.png" style="height: 30px; width: 30px;"></i></td>
+								<td>My Doctor Hospitals(pvt)Ltd<br>Anuradhapura<br>Sri Lanka</td>
+							</tr>
+							<tr style="line-height: 1em;">
+								<td><i class="fa"><img src="icon/6.png" style="height: 30px; width: 30px;"></i></td>
+								<td>+94-55 5555555</td>
+							</tr>
+							<tr style="line-height: 1em;">
+								<td><i class="fa"><img src="icon/4.png" style="height: 30px; width: 30px;"></i></td>
+								<td><a href="#">www.mydoctorhospitals.abc.lk</a></td>
+							</tr>
+							<tr style="line-height: 1em;">
+								<td><i class="fa"><img src="icon/1.png" style="height: 30px; width: 30px;"></i></td>
+								<td><a href="#">my_doc@abc.lk</a></td>
+							<tr>
+							<tr style="line-height: 1em;">
+								<td><i class="fa"><img src="icon/3.png" style="height: 30px; width: 30px;"></i></td>
+								<td>+94-55 5555555</td>
+							</tr>
+						</table>
+					</div>
+
+					<div class="column7">
+							<div class="row">
+								<div class="links">
+								<h2>Navigate</h2>
+									<a href="index.html" style="font-size: 12px; line-height: 2em;">home</a><br>
+									<a href="services.html" style="font-size: 12px; line-height: 2em;">Services</a><br>
+									<a href="about_us.html" style="font-size: 12px; line-height: 2em;">About Us</a><br>
+									<a href="contact.html" style="font-size: 12px; line-height: 2em;">Contact Us</a><br>
+									<hr>
+								</div>
 							</div>
-			</Footer>
+							<div class="row">
+								<div class="connect">
+									<h2>Find Us:</h2>
+
+									<table>
+										<tr>
+											<td><a href="#"><img src="icon/2.png" style="height: 50px; width: 50px;"></a></td>
+											<td><a href="#"><img src="icon/5.png" style="height: 50px; width: 50px;"></a></td>
+										</tr>
+									</table>
+								</div>
+							</div>
+					</div>
+
+				</div>
+
+			</footer>
 
 
 

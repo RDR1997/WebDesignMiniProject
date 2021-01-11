d4729a760da17471cadb3c8a3ef4683541b23592
Randika Ranatunga <75309593+RDR1997@users.noreply.github.com>
2021-01-11 11:38:28 +0530

    updated

--- admin/addDocConnect.php
+++ admin/addDocConnect.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Registration successfully</title>
+		<link rel="stylesheet" type="text/css" href="../css/addDocConnect.css" >
+	</head>
+	<body>
 <?php
 
 	$fname = $_POST['fname'];
@@ -11,19 +18,26 @@
 	
 	//database connection
 		
-	$conn = new mysqli('localhost','root','','doctors');
+	$conn = new mysqli('localhost','root','','my doctor');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(fname,nic,gender,address,tel,email,DocId,category)values(?,?,?,?,?,?,?,?)");
+		$stmt = $conn->prepare("insert into doctor(fname,nic,gender,address,tel,email,DocId,category)values(?,?,?,?,?,?,?,?)");
 		$stmt ->bind_param("ssssisss",$fname,$nic,$gender,$address,$tel,$email,$DocId,$category);
 		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
+		
+		
 		$stmt->close();
 		$conn->close();
 	}
 
 
 ?>	
+	
+		<h1>Registration successfully...</h1>
+		<a href = "../admindashbord.php"><button >Back to Admin Dashbord</button>
+	
+	
+	</body>
+</html>	

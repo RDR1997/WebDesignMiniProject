f4390a6eb36f0a95d36ca36ebf03420178e335f1
Thinun <75312054+Thinun@users.noreply.github.com>
2021-06-03 15:58:24 +0530

    about page

--- about.php
+++ about.php
@@ -1,53 +1,72 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>My Doctor Hospitals</title>
-		<link rel="stylesheet" href="css/about.css">
-		<link rel="stylesheet" href="css/style.css">
+		<title>About Us</title>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/aboutus.css">
 	</head>
 	<body>
-<!-- navigation bar------------------------------------------------------------------------------------------------------------------------------------- -->
-			<ul>
-				<img src="img/logo.png"alt="logo"style="width:55px;height:55px; padding-left: 10px;"align="left";>
-				<li class="logo">My Doctor Hospitals
-				
-				</li>
-				
-				<li class="login_dropdown"><!--when mouse point put on Login aren on Nav.bar it will drop down more options-->
-					<button class="dropbtn">Login</button>
-					<div class="login_dropdown-content">
-						<a href="Admin_login.php">Login as Admin</a>
-						<a href="Doctor_login.php">Login as Doctor</a>
-						<a href="Patient_login.php">Login as Patient</a>
-					</div>
-				</li>
-				<li class="nav"><a href="contact.php">Contact</a></li>
-				<li class="nav"><a href="about.php">About us</a></li>
-				<li class="nav"><a href="services.php">Services</a></li>
-				<li class="nav"><a href="index.php">Home</a></li>
-			</ul>
-		
-       <div class="header">
-       <div class="main">
-         <h1>about.us...</h1>
-         <p>St. Joseph Hospital is a regional, full-service healthcare system serving the Greater Nashua area, western New Hampshire and northern Massachusetts. We are building on a century of service to meet the growing needs of our community through innovation, leading-edge technology, and the highest-quality medical care, our tradition since 1908. Our medical staff, employees, and volunteers are recognized among the very best by all the standard quality measures and by those who count the most - our patients.</p>
+		<!--Title-->
+		<table  width="1180px";>
+				<tr style="font-size:90px;text-align:left;">
+					<td rowspan ="2"><img src="img/logo.png"alt="logo"style="width:160px;height:140px; padding-left: 10px;"></td>
+					<td style="vertical-align: top; color:#1ca7ec;">My Doctor Hospitals</td>
+				</tr>
 
-         <button class="btn">Read More</button><i class="fas fa-book-open"></i></button>
-       </div>
-       </div>
-	   
-		<!--footer-->
-	   	<footer>
-		
+		</table>
+		<ul>
+
+
+			<li class="login_dropdown"><!--when mouse point put on Login aren on Nav.bar it will drop down more options-->
+				<button class="dropbtn">Login</button>
+				<div class="login_dropdown-content">
+					<a href="Admin_login.php">Login as Admin</a>
+					<a href="doctor_login.php">Login as Doctor</a>
+					<a href="patient_login.php">Login as Patient</a>
+				</div>
+			</li>
+			<li class="nav"><a href="contact.php">Contact</a></li>
+			<li class="nav"><a href="about.php">About us</a></li>
+			<li class="nav"><a href="services.php">Services</a></li>
+			<li class="nav"><a href="index.php">Home</a></li>
+		</ul>
+	<br>
 		<div class="row">
-			
-			<div class="column5">
-				<h1>My Doctor Hospitals(pvt)Ltd</h1>
+			<div class="column">
+		<div class="container">
+			<center>
+				<p>Med Center Health is the area’s premier provider of healthcare services. With more hospitals, more clinics, more doctors, and more specialties across a 12-county area, nobody offers a wider range of care, right here.
+
+Our flagship hospital, The Medical Center at Bowling Green, is a hub of forward thinking and innovation. And no matter where they practice, all of our physicians are driven to achieve the highest possible accreditations, equipping them to employ the latest, innovative approaches to healthcare.For instance, Med Center Health was the first facility in the region to use the precision of MAKO™ Robotic-Arm Assisted Surgery, which creates a 3-D model of hip and knee replacement procedures. When it comes to cardiac care, The Medical Center’s heart surgery quality scores far surpass national averages, including an average patient stay of just four days.
+				</p>
+				<a href="contact.php">
+				<button class="btn">Contact Us</button>
+			</a>
+			</center>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="column">
+		<img src="img/aboutusbackground.jpg"height="545" width="750" align="right">
+
+
+	</div>
+</div>
+<br><br><br>
+
+		<footer>
+
+		<div class="row">
+
+			<div class="column5";>
+				<h1 align="left";>My Doctor Hospitals(pvt)Ltd</h1>
 				<img src="img/logo.png"alt="logo"style="width:150px;height:150px;"align="left";>
 			</div>
 
 			<div class="column6">
-				<h2>Contact Us</h2>
+				<h2 align="left";>Contact Us</h2>
 				<table>
 					<tr style="line-height: 1em;">
 						<td><i class="fa"><img src="icon/7.png" style="height: 30px; width: 30px;"></i></td>
@@ -71,22 +90,22 @@
 					</tr>
 				</table>
 			</div>
-	
+
 			<div class="column7">
 					<div class="row">
 						<div class="links">
 						<h2>Navigate</h2>
-                            <a href="index.php" style="font-size: 12px; line-height: 2em;">home</a><br>
-                            <a href="services.php" style="font-size: 12px; line-height: 2em;">Services</a><br>
-                            <a href="about.php" style="font-size: 12px; line-height: 2em;">About Us</a><br>
-                            <a href="contact.php" style="font-size: 12px; line-height: 2em;">Contact Us</a><br>
+							<a href="index.php" style="font-size: 12px; line-height: 2em;">home</a><br>
+							<a href="services.php" style="font-size: 12px; line-height: 2em;">Services</a><br>
+							<a href="about.php" style="font-size: 12px; line-height: 2em;">About Us</a><br>
+							<a href="contact.php" style="font-size: 12px; line-height: 2em;">Contact Us</a><br>
 							<hr>
 						</div>
 					</div>
 					<div class="row">
 						<div class="connect">
 							<h2>Find Us:</h2>
-					
+
 							<table>
 								<tr>
 									<td><a href="#"><img src="icon/2.png" style="height: 50px; width: 50px;"></a></td>
@@ -98,7 +117,8 @@
 			</div>
 
 		</div>
-	
-	</footer><!--footer end-->
-   </body>
+
+		</footer>
+
+</body>
 </html>

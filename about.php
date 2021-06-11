2270a05258c57103b2c594094a633890586fbb31
Dinithikw <Dinithikw@users.noreply.github.com>
2021-06-11 13:11:32 +0530

    anout us updated

--- about.php
+++ about.php
@@ -2,58 +2,43 @@
 <html>
 	<head>
 		<title>About Us</title>
-		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<!--<link rel="stylesheet" type="text/css" href="css/index.css">-->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/aboutus.css">
 	</head>
 	<body>
 		<!--Title-->
-		<table  width="1180px";>
-				<tr style="font-size:90px;text-align:left;">
-					<td rowspan ="2"><img src="img/logo.png"alt="logo"style="width:160px;height:140px; padding-left: 10px;"></td>
-					<td style="vertical-align: top; color:#1ca7ec;">My Doctor Hospitals</td>
-				</tr>
-
-		</table>
 		<ul>
-
-
-			<li class="login_dropdown"><!--when mouse point put on Login aren on Nav.bar it will drop down more options-->
-				<button class="dropbtn">Login</button>
-				<div class="login_dropdown-content">
-					<a href="Admin_login.php">Login as Admin</a>
-					<a href="doctor_login.php">Login as Doctor</a>
-					<a href="patient_login.php">Login as Patient</a>
-				</div>
-			</li>
-			<li class="nav"><a href="contact.php">Contact</a></li>
-			<li class="nav"><a href="about.php">About us</a></li>
-			<li class="nav"><a href="services.php">Services</a></li>
-			<li class="nav"><a href="index.php">Home</a></li>
-		</ul>
+				<img src="img/logo.png"alt="logo"style="width:55px;height:55px; padding-left: 10px;"align="left";>
+				<li class="logo">My Doctor Hospitals
+				
+				</li>
+				
+				<li class="login_dropdown"><!--when mouse point put on Login aren on Nav.bar it will drop down more options-->
+					<button class="dropbtn">Login</button>
+					<div class="login_dropdown-content">
+						<a href="Admin_login.php">Login as Admin</a>
+						<a href="Doctor_login.php">Login as Doctor</a>
+						<a href="Patient_login.php">Login as Patient</a>
+					</div>
+				</li>
+				<li class="nav"><a href="contact.php">Contact</a></li>
+				<li class="nav"><a href="about.php">About us</a></li>
+				<li class="nav"><a href="services.php">Services</a></li>
+				<li class="nav"><a href="index.php">Home</a></li>
+			</ul>
 	<br>
 		<div class="row">
-			<div class="column">
-		<div class="container">
-			<center>
-				<p>Med Center Health is the area’s premier provider of healthcare services. With more hospitals, more clinics, more doctors, and more specialties across a 12-county area, nobody offers a wider range of care, right here.
-
-Our flagship hospital, The Medical Center at Bowling Green, is a hub of forward thinking and innovation. And no matter where they practice, all of our physicians are driven to achieve the highest possible accreditations, equipping them to employ the latest, innovative approaches to healthcare.For instance, Med Center Health was the first facility in the region to use the precision of MAKO™ Robotic-Arm Assisted Surgery, which creates a 3-D model of hip and knee replacement procedures. When it comes to cardiac care, The Medical Center’s heart surgery quality scores far surpass national averages, including an average patient stay of just four days.
-				</p>
-				<a href="contact.php">
-				<button class="btn">Contact Us</button>
-			</a>
-			</center>
+			<div class="column_left">
+				Med Center Health is the area’s premier provider of healthcare services. With more hospitals, more clinics, more doctors, and more specialties across a 12-county area, nobody offers a wider range of care, right here.
+				
+				Our flagship hospital, The Medical Center at Bowling Green, is a hub of forward thinking and innovation. And no matter where they practice, all of our physicians are driven to achieve the highest possible accreditations, equipping them to employ the latest, innovative approaches to healthcare.For instance, Med Center Health was the first facility in the region to use the precision of MAKO™ Robotic-Arm Assisted Surgery, which creates a 3-D model of hip and knee replacement procedures. When it comes to cardiac care, The Medical Center’s heart surgery quality scores far surpass national averages, including an average patient stay of just four days.
+				
+			</div>
+			<div class="column_right">
+				<img src="img/aboutusbackground.jpg" width="100%" height="auto" align="right">
+			</div>
 		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="column">
-		<img src="img/aboutusbackground.jpg"height="545" width="750" align="right">
-
-
-	</div>
-</div>
 <br><br><br>
 
 		<footer>

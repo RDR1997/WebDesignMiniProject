4b11bbc65f2a4146893f07cac5efc5c9894b429e
Dinithikw <Dinithikw@users.noreply.github.com>
2021-06-11 19:40:25 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,6 +5,22 @@
 		<link rel="stylesheet" href="css/contact.css">
 		<link rel="stylesheet" href="css/style.css">
 	</head>
+	<?php
+		if (isset($_POST['submit'])){
+		$name = $_POST['name'];
+		$visitor_email = $_POST['email'];
+		$message = $_POST['message'];
+		$subject = $_POST['subject'];
+		
+		$to="dskapuwatte@yahoo.com";
+		$headers = "From: ".$visitor_email;
+		$email_body="User name:".$name. "User email:".$visitor_email. "User message:".$message; 
+		
+		mail($to,$subject,$email_body,$headers) or die("Error!");
+		
+		header("Location:contact.php?mailsend");
+		}	
+	?>
 	<body>
 <!-- navigation bar------------------------------------------------------------------------------------------------------------------------------------- -->
 			<ul>
@@ -32,7 +48,7 @@
 	<!--<div class="col-md-1">
 	</div>-->
 		<div class="column1">
-				<form action="#">
+				<form action="contact.php">
 					<fieldset>
 						<legend><h3>Send your feedback</h3></legend>
 						
